7e0c9cb8cc96ebc26834a67f077e369928d997f6
Luke265 <n.lukas265@gmail.com>
2020-07-23 14:36:56 +0000

    Select screen to refresh

--- src/robot/screen.ts
+++ src/robot/screen.ts
@@ -3,6 +3,7 @@ import { Finder } from '../finder/finder';
 import { Robot } from "./robot";
 import * as cv from 'opencv4nodejs';
 import { Result } from "../finder/result";
+import { Mat } from "opencv4nodejs";
 
 export class Screen {
 
@@ -13,13 +14,13 @@ export class Screen {
 
     }
 
-    refresh() {
+    refresh(screen: number = 0, mat?: Mat) {
         if (!this.capture) {
             this.capture = new Capture();
             const [width, height] = getDesktopResolution();
             this.mat = new cv.Mat(Math.ceil(height / 8) * 8, Math.ceil(width / 8) * 8, cv.CV_8UC3);
         }
-        return this.capture.grab(this.mat, 0);
+        return this.capture.grab(mat || this.mat, screen);
     }
 
     async untilFound(finder: Finder, timeout?: number, delay?: number) {
